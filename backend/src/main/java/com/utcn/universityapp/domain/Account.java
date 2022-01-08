22971a18823fc2fb33fe3070baaab637a652b0f5
Stefan923 <stefanpopescu923@gmail.com>
2022-01-08 16:12:15 +0200

    Updated the Account class to extend UserDetails

--- backend/src/main/java/com/utcn/universityapp/domain/Account.java
+++ backend/src/main/java/com/utcn/universityapp/domain/Account.java
@@ -3,14 +3,17 @@ package com.utcn.universityapp.domain;
 import com.utcn.universityapp.domain.user.User;
 import lombok.Data;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Data
 @Entity
 @Table(name = "account")
 @EnableAutoConfiguration
-public class Account {
+public class Account implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,9 +29,40 @@ public class Account {
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "locked", nullable = false, columnDefinition = "boolean default false")
+    private boolean locked;
+
+    @Column(name = "enabled", nullable = false, columnDefinition = "boolean default false")
+    private boolean enabled;
+
     @OneToOne
     @MapsId
     @JoinColumn(name = "userId")
     private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 
 }
