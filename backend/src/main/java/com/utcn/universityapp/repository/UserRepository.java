37d2b30b0c8087bd2a508c6cf92d9cb8662148a8
Stefan923 <stefanpopescu923@gmail.com>
2022-01-12 01:58:08 +0200

    Search user by firstName and lastName fields

--- backend/src/main/java/com/utcn/universityapp/repository/UserRepository.java
+++ backend/src/main/java/com/utcn/universityapp/repository/UserRepository.java
@@ -1,11 +1,23 @@
 package com.utcn.universityapp.repository;
 
 import com.utcn.universityapp.domain.user.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @RestResource(path = "byName", rel = "findUsersByName")
+    <S extends User> List<S> findAllByFirstNameContainingIgnoreCaseAndLastNameContainingIgnoreCase(
+            @RequestParam("firstName") String firstName,
+            @RequestParam("lastName")  String lastName,
+            Pageable pageable);
+
 }
