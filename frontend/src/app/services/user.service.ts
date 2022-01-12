07e363d08302362e2561f8abda6519d603d69df3
Stefan923 <stefanpopescu923@gmail.com>
2022-01-12 04:06:27 +0200

    Added user service

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { map } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private static readonly BASE_URL = "http://localhost:8080";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsersByName(firstName: string, lastName: string, categoryId: number, page: number, pageSize: number): Observable<GetResponseUsers> {
+    const url = `${UserService.BASE_URL}/users/search/byName`
+      + `?firstName=${firstName}&lastName=${lastName}&page=${page}&size=${pageSize}`;
+
+    return this.httpClient.get<GetResponseUsers>(url);
+  }
+
+  getUsersByRoleId(roleId: number, page: number, pageSize: number): Observable<GetResponseUsers> {
+    const url = `${UserService.BASE_URL}/products/search/byRoleId`
+      + `?roleId=${roleId}}&page=${page}&size=${pageSize}`;
+
+    return this.httpClient.get<GetResponseUsers>(url);
+  }
+
+  getUsersByUsername(username: string, page: number, pageSize: number): Observable<GetResponseUsers> {
+    const url = `${UserService.BASE_URL}/products/search/byUsername`
+      + `?username=${username}}&page=${page}&size=${pageSize}`;
+
+    return this.httpClient.get<GetResponseUsers>(url);
+  }
+
+  getUserById(userId: number): Observable<User> {
+    const url = `${UserService.BASE_URL}/users/${userId}`;
+
+    return this.httpClient.get<User>(url);
+  }
+
+}
+
+export interface GetResponseUsers {
+  _embedded: {
+    users: User[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
+  }
+}
