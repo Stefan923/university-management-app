ffb9ddeb98d6a75cf67ca2e7a65f6f5675a62381
Stefan923 <stefanpopescu923@gmail.com>
2022-01-12 12:21:07 +0200

    Added search StudentSubject by student username

--- backend/src/main/java/com/utcn/universityapp/repository/StudentSubjectRepository.java
+++ backend/src/main/java/com/utcn/universityapp/repository/StudentSubjectRepository.java
@@ -3,9 +3,22 @@ package com.utcn.universityapp.repository;
 import com.utcn.universityapp.domain.subject.StudentSubject;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "student_subjects", path = "student_subjects")
 public interface StudentSubjectRepository extends JpaRepository<StudentSubject, Long> {
+
+    @RestResource(path = "byTeacherUsername", rel = "findStudentSubjectsByTeacherUsername")
+    List<StudentSubject> findAllByTeacherUserAccountUsernameLike(
+            @RequestParam("username") String username);
+
+    @RestResource(path = "byStudentUsername", rel = "findStudentSubjectsByStudentUsername")
+    List<StudentSubject> findAllByStudentUserAccountUsernameLike(
+            @RequestParam("username") String username);
+
 }
